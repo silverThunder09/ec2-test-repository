ff3cd926bb480a1cdde1d92a89db60dc08ca3157
silverThunder09 <jeg9017@gmail.com>
2026-05-18 17:29:06 +0900

    feat: GitHub Actions 기반 CI/CD 배포 환경 구성
    - main 브랜치 푸시 시 테스트, Docker 이미지 빌드, Docker Hub 푸시, EC2 배포가 수행되도록 워크플로우를 추가

--- src/test/java/com/ec2test/Ec2TestApplicationTests.java
+++ src/test/java/com/ec2test/Ec2TestApplicationTests.java
@@ -1,9 +1,11 @@
 package com.ec2test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Ec2TestApplicationTests {
 
     @Test
--- src/test/java/com/ec2test/service/MemberServiceTest.java
+++ src/test/java/com/ec2test/service/MemberServiceTest.java
@@ -11,16 +11,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.hibernate.ddl-auto=create-drop"
-})
+@ActiveProfiles("test")
 class MemberServiceTest {
 
     @Autowired
